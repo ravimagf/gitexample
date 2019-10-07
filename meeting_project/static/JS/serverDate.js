3b97d3826212d3540747259a9201429f6b5e0765
ravimagf <55493583+ravimagf@users.noreply.github.com>
2019-10-07 14:37:34 +0530

    my first commit

---
+++ meeting_project/static/JS/serverDate.js
@@ -0,0 +1,7 @@
+function showInput() {
+        document.getElementById('display').innerHTML =
+                    document.getElementById("user_input").value;
+
+        document.getElementById('time').innerHTML = new Date();
+
+        }
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ravikumar2\PycharmProjects\meeting_projedt\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
